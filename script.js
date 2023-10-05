9d8e564aede3b878f00cde5a4982fcdbd70f2ad3
Luke Shen <145064698+imlukeshen@users.noreply.github.com>
2023-10-05 10:02:15 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let sections = document.querySelectorAll('section');
+    let navLinks = document.querySelectorAll('header nav a');
+
+    window.addEventListener('scroll', function() {
+        sections.forEach(sec => {
+            let top = window.scrollY;
+            let offset = sec.offsetTop - 100;
+            let height = sec.offsetHeight;
+            let id = sec.getAttribute('id');
+
+            if (top >= offset && top < offset + height) {
+                navLinks.forEach(links => {
+                    links.classList.remove('active');
+                    document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+                });
+                //active sections for animation on scroll
+                sec.classList.add('show-animate');
+            }
+
+            else {
+                sec.classList.remove('show-animate');
+            }
+        });
+
+        let header = document.querySelector('header');
+        header.classList.toggle('sticky', window.scrollY > 100);
+
+        //remove toggle icon and navbar when click navbar links (scroll)
+        MediaElementAudioSourceNode.classList.remove('bx-x');
+        navbar.classList.remove('active');
+
+    });
+});
