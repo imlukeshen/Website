da65f4a66689184b37c480af601daf283a250e35
Luke Shen <145064698+imlukeshen@users.noreply.github.com>
2023-10-25 21:25:08 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
+
+    let menuIcon = document.querySelector('#menu-icon');
+    let navbar = document.querySelector('.navbar');
+
+    menuIcon.onclick = () => {
+        menuIcon.classList.toggle('bx-x');
+        navbar.classList.toggle('active');
+    }
+
     let sections = document.querySelectorAll('section');
     let navLinks = document.querySelectorAll('header nav a');
 
